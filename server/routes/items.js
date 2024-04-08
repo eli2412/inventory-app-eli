325404ddb7516ffc620dc294178f5c5445fd1b80
eli2412 <elias.cruz49@yahoo.com>
2024-04-08 12:29:56 -0700

    doc add id for item

--- server/routes/items.js
+++ server/routes/items.js
@@ -11,5 +11,16 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+router.get("/:id", async (req, res, next) => {
+  try {
+      const items = await Item.findByPk(req.params.id);
+      if (!items) {
+          throw new Error("No show found")
+      }
+      res.json(items)
+  } catch (error) {
+      next(error)
+  }
+})
 
 module.exports = router;
