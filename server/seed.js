72636a6c570ab27f1394c2889d49c2ed46160265
Chemeeka Bahe <bchemeeka@gmail.com>
2024-04-08 11:08:56 -0700

    docs: imported Items into the seed file

--- server/seed.js
+++ server/seed.js
@@ -2,6 +2,7 @@ const {sauces, items} = require('./seedData.js');
 
 const {sequelize} = require('./db');
 const {Sauce} = require('./models');
+const { Item } = require('./models')
 
 const seed = async () => {
 
@@ -11,6 +12,7 @@ const seed = async () => {
     
         // insert data
         await Promise.all(sauces.map(sauce => Sauce.create(sauce)));
+        await Promise.all(items.map(items => Item.create(items)));
 
         console.log("db populated!");
     } catch (error) {
