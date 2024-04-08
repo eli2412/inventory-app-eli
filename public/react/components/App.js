63b1f64fe5ce6c7eea540fe2b2a7f4317d4968d0
JayEdwards-QuantLead <jedwards11236@gmail.com>
2024-04-08 15:39:30 -0400

    React App add Items

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { ItemsList } from './ItemsList'
 import { SaucesList } from './SaucesList';
 
 // import and prepend the api url to any fetch calls
@@ -28,6 +29,31 @@ export const App = () => {
       <h1>Sauce Store</h1>
 			<h2>All things 🔥</h2>
 			<SaucesList sauces={sauces} />
+		</main>
+	)
+
+	const [items, setItems] = useState([]);
+
+	async function fetchItems(){
+		try {
+			const response = await fetch(`${apiURL}/items`);
+			const itemData = await response.json();
+			
+			setSauces(itemData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+	useEffect(() => {
+		fetchItems();
+	}, []);
+
+	return (
+		<main>	
+      <h1>Item Store</h1>
+			<h2>All things 🔥</h2>
+			<ItemsList items={items} />
 		</main>
 	)
 }
