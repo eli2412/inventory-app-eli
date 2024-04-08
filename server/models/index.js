a4f7559fa4923fff027d6f6a2e8f7191af1d77e6
JayEdwards-QuantLead <jedwards11236@gmail.com>
2024-04-08 13:05:31 -0400

    Sequelize Model for Items

--- server/models/index.js
+++ server/models/index.js
@@ -6,6 +6,14 @@ const Sauce = sequelize.define("sauces", {
   image: Sequelize.STRING,
 });
 
+const Item = sequelize.define("items", {
+  name: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  description: Sequelize.STRING,
+  category: Sequelize.STRING
+  image: Sequelize.STRING,
+});
+
 module.exports = {
   db: sequelize,
   Sauce,
